dae72cba59951700479eef6e7a5aabc08ece1601
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-05-06 18:43:05 +0430

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,15 @@
+import kotlin.system.exitProcess
+
+/**
+ * The *man* program on linux looks for manual pages of a program in _/usr/man_
+ * directory. So, you should copy your man files in there at installation time.
+ */
+fun main(args: Array<String>) {
+    if (args.contains("--help")) {
+        println("This is the help to show to users.")
+        exitProcess(0)
+    } else if (args.contains("--version")) {
+        println("v1.0.0")
+        exitProcess(0)
+    }
+}
